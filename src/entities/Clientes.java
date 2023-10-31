e5d9474d91be49460e624f26f28ff58e4e49dc5f
Mendes44 <marcosmendesm10@gmail.com>
2023-10-31 09:18:30 -0300

    Parte Cliente Finalizada

--- src/entities/Clientes.java
+++ src/entities/Clientes.java
@@ -7,4 +7,47 @@ public class Clientes {
 	private String nome;
 	private String email;
 	private Date dataNiver;
+	
+	public Clientes() {
+		
+	}
+
+	public Clientes(String nome, String email, Date dataNiver) {
+		this.nome = nome;
+		this.email = email;
+		this.dataNiver = dataNiver;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataNiver() {
+		return dataNiver;
+	}
+
+	public void setDataNiver(Date dataNiver) {
+		this.dataNiver = dataNiver;
+	}
+
+	@Override
+	public String toString() {
+		return "Clientes [nome=" + nome + ", email=" + email + ", dataNiver=" + dataNiver + "]";
+	}
+	
+	
+	
+	
 }
