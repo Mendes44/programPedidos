2280350991a8d43195df1839a70b6aa5743f3bb0
Mendes44 <marcosmendesm10@gmail.com>
2023-10-31 09:20:30 -0300

    Produtos Finalizado

--- src/entities/Produtos.java
+++ src/entities/Produtos.java
@@ -5,5 +5,30 @@ public class Produtos {
 	private String nomeProduto;
 	private double preco;
 	
+	public Produtos () {
+		
+	}
+
+	public Produtos(String nomeProduto, double preco) {
+		this.nomeProduto = nomeProduto;
+		this.preco = preco;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
 	
 }
